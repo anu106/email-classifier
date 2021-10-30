60252bacd501d87c33b6f5e27e6892ce55a76dc2
anu106 <79446255+anu106@users.noreply.github.com>
2021-10-31 02:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 from flask import Flask, render_template, request, url_for
-from flask_bootstrap import Bootstrap
 from textblob import TextBlob
 
 application = app = Flask(__name__)
